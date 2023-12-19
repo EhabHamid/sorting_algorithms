c6b4c81826ee8fd0e49adf59985502b0453d6ec0
EhabHamid <ehabhamidalx@gmail.com>
2023-12-19 15:03:14 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -1,65 +1,27 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/* comment */
-#define UP 0
-#define DOWN 1
-
+#include <stddef.h>
 
 /**
- * struct listint_s - comment
- * @n: comment
- * @prev: comment
- * @next: comment
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
- 
 typedef struct listint_s
 {
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
-
-
-/**
- * enum bool - comment
- * @false: comment
- * @true: comment
- */
- 
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
-
-
-/* comment */
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-
-
-
-/* comment */
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void quick_sort_hoare(int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
-
-
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 #endif
-
